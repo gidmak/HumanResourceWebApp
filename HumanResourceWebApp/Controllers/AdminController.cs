10719f272a1f8b18241599d6b9a84b2713b6ee75
gidmak <gideonmarkus@gmail.com>
2020-09-01 05:24:31 +0100

    Create admin

--- HumanResourceWebApp/Controllers/AdminController.cs
+++ HumanResourceWebApp/Controllers/AdminController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using HumanResourceWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HumanResourceWebApp.Controllers
 {
     public class AdminController : Controller
     {
         private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Admin Admins { get; set; }
         public AdminController(ApplicationDbContext db)
         {
             _db = db;
@@ -18,5 +21,51 @@ namespace HumanResourceWebApp.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Admins = new Admin();
+            if(id == null)
+            {
+                //Create a new admin
+                return View(Admins);
+            }
+            //Update admin
+            Admins = await _db.Admins.FirstOrDefaultAsync(u => u.Id == id);
+            if(Admins == null)
+            {
+                return NotFound();
+            }
+            return View(Admins);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if(Admins.Id == 0)
+                {
+                    //Create new admin record
+                    _db.Admins.Add(Admins);
+                }
+                else
+                {
+                    //Update admin record
+                    _db.Admins.Update(Admins);
+                }
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(Admins);
+        }
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Admins.ToListAsync() });
+        }
+        #endregion
     }
 }
---
+++ HumanResourceWebApp/wwwroot/js/adminList.js
@@ -0,0 +1,62 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_Load').DataTable({
+        "ajax": {
+            "url": "/admin/getall",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "firstName", "width": "15%" },
+            { "data": "middleName", "width": "15%" },
+            { "data": "lastName", "width": "15%" },
+            { "data": "email", "width": "25%" },
+            { "data": "Password", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                            <a href="/Admin/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>Edit</a>
+                            &nbsp;
+                            <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;' onclick=Delete('/admin/Delete?id='+${data})>Delete</a>
+                            </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    })
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once delete, you cannot recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
